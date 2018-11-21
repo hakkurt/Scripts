e04b419d5c9c5707f7a33b228aa5c72a88589cf3
Hakan Akkurt <hakkurt@gmail.com>
2018-11-21 19:21:45 +0100

    Update VDStoNVDS-migration.ps1

--- NSX-T/VDStoNVDS-migration.ps1
+++ NSX-T/VDStoNVDS-migration.ps1
@@ -1,7 +1,6 @@
 $VCConn = Connect-VIServer -Server vcsa-01a.corp.local -user administrator@vsphere.local -password VMware1!
 
 $vm = Get-VM -name "TestVM02"
-$lstest = Get-View -ViewType OpaqueNetwork | ? {$_.name -eq "LS-Test"}
 
 $networkAdapter = Get-NetworkAdapter -VM $vm 
 
@@ -10,4 +9,8 @@ $destinationPortGroup = Get-VDPortgroup -VDSwitch VDS-Test -Name DummyPG
 
 Move-VM -VM $vm -Destination $destination -NetworkAdapter $networkAdapter -PortGroup $destinationPortGroup
 
-$vm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $lstest.Name -confirm:$false -confirm:$false
+$lstest = Get-View -ViewType OpaqueNetwork | ? {$_.name -eq "LS-Test"}
+$null = $vm | Get-NetworkAdapter -name  "Network adapter 1" | Set-NetworkAdapter -NetworkName $lstest.Name -confirm:$false
+
+
+Disconnect-VIServer -confirm:$false
