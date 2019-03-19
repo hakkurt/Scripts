34b6ffa6ab92b266ac6dba4036761738be152623
Hakan Akkurt <hakkurt@gmail.com>
2019-03-19 08:02:21 +0100

    Add files via upload

---
+++ Test-VTEP-Conn/Test-VTEP-Conn.ps1
@@ -0,0 +1,45 @@
+
+$vcenter= "vCenter"
+$user = "administrator@vsphere.local"
+$pass = "pass"
+$sourcehost = "esx1"
+
+$Server=Connect-VIServer -Server $vcenter -User $user -Password $pass -WarningAction SilentlyContinue
+
+$Clusters = Get-Cluster | Sort Name
+
+write-host -ForegroundColor green "Cluster	-	Host	-	VMKernel	-	VMKernel IP	-	Packet Loss"
+
+	ForEach ($Cluster in $Clusters){
+
+	$VmHosts = $Cluster | Get-VmHost | Where {$_.ConnectionState -eq "Connected"} | Sort Name
+
+		ForEach ($VmHost in $VmHosts){
+
+			$VMKernels = Get-VMHostNetworkAdapter -VMHost $VmHost -VMKernel
+			
+				ForEach ($VmKernel in $VMKernels){
+				
+					if(!$VMKernel.VMotionEnabled)  {     
+						if(!$VMKernel.ManagementTrafficEnabled)		{			
+						
+						$esxcli = Get-ESXCLI -vmhost $sourcehost -V2 
+
+						$arguments = $esxcli.network.diag.ping.CreateArgs()
+						$arguments.host = $VMKernel.IP
+						$arguments.count = 3
+						$arguments.netstack = "vxlan"
+						$arguments.df = $true
+						$arguments.interface = "vmk3"
+						$arguments.size = "1600"
+						$pingStatus = $esxcli.network.diag.ping.Invoke($arguments)
+							
+							write-host $Cluster "-" $VMHost "-" $VMKernel.Name "-" $VMKernel.IP "-" $pingStatus.summary.PacketLost
+						}
+					} 
+
+				}
+		}
+	}
+
+Disconnect-VIServer -Server $Server -Confirm:$false
